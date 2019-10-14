825960fb7f4dc3f76222f8aa50878b3cff923176
felipe <fcpereira@dcomp.ufs.br>
2019-10-14 02:11:20 -0300

    Incorporação do algoritmo para o RL2 e primeira etapa do método subgradiente

--- agmr_rl.cpp
+++ agmr_rl.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include "agmr_rl1.h"
+#include "agmr_rl2.h"
 
 using namespace std;
 
@@ -22,6 +23,29 @@ void load_edges(FILE* input_file, int n_edges, vector<pair<int,int>> *edges, vec
 	}
 }
 
+// Sets the initial values of lambdas variables
+void set_lambdas(int n_vertices, vector<int> *vertices_degrees, vector<double> *vertices_lambdas)
+{
+	int max_degree = 0;
+
+	for (int i = 0; i < n_vertices; ++i)
+	{
+		max_degree = max(max_degree, (*vertices_degrees)[i]);
+	}
+
+	for (int i = 0; i < n_vertices; i++)
+	{
+		if((*vertices_degrees)[i] <= 2)
+		{
+			(*vertices_lambdas)[i] = 0;
+		}
+		else
+		{
+			(*vertices_lambdas)[i] = 1.0 / (double) max_degree;
+		}
+	}
+}
+
 int main (int argc, char *argv[])
 {
 	// General variables
@@ -29,6 +53,7 @@ int main (int argc, char *argv[])
 	FILE *input_file;
 	FILE *output_file;
 	int n_vertices, n_edges;
+	double rl1_sol_value, rl2_sol_value, rl_sol_value;
 
 	// Opens input and output files
 	input_path = argv[1];
@@ -43,10 +68,25 @@ int main (int argc, char *argv[])
 	load_edges(input_file, n_edges, &edges, &vertices_degrees);
 
 	// Vector of lambdas of langrarian relaxation
-	vector<int> vertices_lambdas(n_vertices, 1);
+	vector<double> vertices_lambdas(n_vertices);
+	set_lambdas(n_vertices, &vertices_degrees, &vertices_lambdas);
 
-	// Executes Kruskal algorithm for getting a minimum spanning tree
-	kruskal(n_vertices, n_edges, &edges, &vertices_lambdas);
+	// Executes Kruskal algorithm for getting a minimum spanning tree for RL1 problem
+	rl1_sol_value = kruskal(n_vertices, n_edges, &edges, &vertices_lambdas);
+
+	// Executes inspection algorithm for RL2 problem
+	rl2_sol_value = inspection(n_vertices, &vertices_degrees, &vertices_lambdas);
+
+	// Calculates RL solution value
+	rl_sol_value = 0;
+	for (int i = 0; i < n_vertices; i++)
+	{
+		rl_sol_value += vertices_lambdas[i];
+	}
+	rl_sol_value = (-2) * rl_sol_value;
+	rl_sol_value += rl1_sol_value + rl2_sol_value;
+
+	cout << rl1_sol_value << " " << rl2_sol_value << " " << rl_sol_value << endl;
 
 	// Closes input and output files
 	fclose(input_file);
