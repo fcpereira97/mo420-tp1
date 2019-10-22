e2d99ecd3414e181bb36587d2c154f353139e2b9
felipe <fcpereira@dcomp.ufs.br>
2019-10-21 22:48:16 -0300

    Adição de heurística IMP2

--- heuristic.cpp
+++ heuristic.cpp
@@ -60,8 +60,6 @@ void find_path(Vertex *v1, Vertex *v2, list<Edge*> *path)
 			next_v1 = (*incident_edge)-> vertex_2;
 		if(!next_v1-> visited)
 		{
-			//cout << v1->incident_edges.size() << endl;
-			//cout << v1-> index << " " << next_v1 -> index << " " << v2-> index << endl;
 			(*path).push_back(*incident_edge);
 
 			if(next_v1 -> index != v2-> index)
@@ -81,37 +79,142 @@ void find_path(Vertex *v1, Vertex *v2, list<Edge*> *path)
 	v1-> visited = false;
 }
 
-void change_edges(Edge *in_edge, list<Edge*> *path)
+void change_edges(Edge *in_edge, Edge *out_edge)
 {
-	Edge *out_edge = NULL;
+	out_edge-> variable = false;
+	out_edge-> vertex_1-> incident_edges.remove(out_edge);
+	out_edge-> vertex_2-> incident_edges.remove(out_edge);
+	out_edge-> vertex_1-> ramifications--;
+	out_edge-> vertex_2-> ramifications--;
+	out_edge-> vertex_1-> variable = out_edge-> vertex_1-> ramifications > 2;
+	out_edge-> vertex_2-> variable = out_edge-> vertex_2-> ramifications > 2;
 
-	for (list<Edge*>::iterator cand_edge = (*path).begin(); cand_edge != (*path).end(); ++cand_edge)
-	{
-		if((*cand_edge)-> vertex_1 -> ramifications == 3 && (*cand_edge)-> vertex_2 -> ramifications == 3)
-			out_edge = (*cand_edge);
-		else if((*cand_edge)-> vertex_1 -> ramifications == 3 || (*cand_edge)-> vertex_2 -> ramifications == 3)
-			out_edge = (*cand_edge);
-	}
-
-	if (out_edge != NULL)
-	{
-		out_edge-> variable = false;
-		out_edge-> vertex_1 -> incident_edges.remove(out_edge);
-		out_edge-> vertex_2 -> incident_edges.remove(out_edge);
-		out_edge-> vertex_1 -> ramifications--;
-		out_edge-> vertex_2 -> ramifications--;
-		out_edge-> vertex_1 -> variable = out_edge-> vertex_1 -> ramifications > 2;
-		out_edge-> vertex_2 -> variable = out_edge-> vertex_2 -> ramifications > 2;
-
-		in_edge -> variable = true;
-		in_edge -> vertex_1 -> incident_edges.push_back(in_edge);
-		in_edge -> vertex_2 -> incident_edges.push_back(in_edge);
-		in_edge-> vertex_1 -> ramifications++;
-		in_edge-> vertex_2 -> ramifications++;
-		in_edge-> vertex_1 -> variable = in_edge-> vertex_1 -> ramifications > 2;
-		in_edge-> vertex_2 -> variable = in_edge-> vertex_2 -> ramifications > 2;
-	}
+	in_edge-> variable = true;
+	in_edge-> vertex_1-> incident_edges.push_back(in_edge);
+	in_edge-> vertex_2-> incident_edges.push_back(in_edge);
+	in_edge-> vertex_1-> ramifications++;
+	in_edge-> vertex_2-> ramifications++;
+	in_edge-> vertex_1-> variable = in_edge-> vertex_1-> ramifications > 2;
+	in_edge-> vertex_2-> variable = in_edge-> vertex_2-> ramifications > 2;
 }
+
+bool improvement_1(list<Edge*> *available_edges)
+{	
+	bool improved = false;
+	list<Edge*> available_edges_aux;
+
+	for(list<Edge*>::iterator available_edge = (*available_edges).begin(); available_edge != (*available_edges).end(); ++available_edge)
+	{
+		Vertex *v1, *v2;
+		Edge* in_edge = *available_edge;
+		v1 = in_edge-> vertex_1;
+		v2 = in_edge-> vertex_2;
+
+		if(v1-> ramifications != 2 && v2->ramifications != 2)
+		{
+			list<Edge*> path;
+			find_path(v1, v2, &path);
+			Edge *out_edge = NULL;
+
+			for (list<Edge*>::iterator candidate_out_edge = path.begin(); candidate_out_edge != path.end(); ++candidate_out_edge)
+			{
+				if((*candidate_out_edge)-> vertex_1 -> ramifications == 3 &&
+				   (*candidate_out_edge)-> vertex_1 -> index != v1-> index &&
+				   (*candidate_out_edge)-> vertex_1 -> index != v2-> index
+				   ||
+				   (*candidate_out_edge)-> vertex_2 -> ramifications == 3 &&
+				   (*candidate_out_edge)-> vertex_2 -> index != v1-> index &&
+				   (*candidate_out_edge)-> vertex_2 -> index != v2-> index)
+				{
+					out_edge = (*candidate_out_edge);
+					break;
+				}
+			}
+			if (out_edge != NULL)
+			{
+				improved = true;
+				change_edges(in_edge, out_edge);
+				available_edges_aux.push_back(out_edge);
+			}
+			else
+			{
+				available_edges_aux.push_back(in_edge);
+			}
+		}
+	}
+
+	(*available_edges).clear();
+	(*available_edges).assign(available_edges_aux.begin(), available_edges_aux.end());
+
+	return improved;
+}
+
+bool improvement_2(list<Edge*> *available_edges)
+{	
+	bool improved = false;
+	list<Edge*> available_edges_aux;
+
+	for(list<Edge*>::iterator available_edge = (*available_edges).begin(); available_edge != (*available_edges).end(); ++available_edge)
+	{
+		Vertex *v1, *v2;
+		Edge* in_edge = *available_edge;
+		v1 = in_edge-> vertex_1;
+		v2 = in_edge-> vertex_2;
+
+		if(v1-> ramifications != 2 || v2-> ramifications != 2)
+		{
+			list<Edge*> path;
+			find_path(v1, v2, &path);
+			Edge *out_edge = NULL;
+
+			if(v1-> ramifications != 2)
+			{
+				Vertex *v3;
+				if(path.back()-> vertex_1 -> index != v2-> index)
+					v3 = path.back()-> vertex_1;
+				else
+					v3 = path.back()-> vertex_2;
+
+				if(v3-> ramifications == 3)
+				{
+					out_edge = path.back();
+				}
+			}
+
+
+			if(v2-> ramifications != 2)
+			{
+				Vertex *v3;
+				if(path.front()-> vertex_1 -> index != v1-> index)
+					v3 = path.front()-> vertex_1;
+				else
+					v3 = path.front()-> vertex_2;
+
+				if(v3-> ramifications == 3)
+				{
+					out_edge = path.front();
+				}
+			}
+
+			if (out_edge != NULL)
+			{
+				improved = true;
+				change_edges(in_edge, out_edge);
+				available_edges_aux.push_back(out_edge);
+			}
+			else
+			{
+				available_edges_aux.push_back(in_edge);
+			}
+		}
+	}
+
+	(*available_edges).clear();
+	(*available_edges).assign(available_edges_aux.begin(), available_edges_aux.end());
+
+	return improved;
+}
+
 
 int heuristic(int n_vertices, int n_edges, vector<Vertex*> *vertices, vector<Edge*> *edges)
 {
@@ -137,28 +240,17 @@ int heuristic(int n_vertices, int n_edges, vector<Vertex*> *vertices, vector<Edg
 
 		if((*edges)[i]-> variable)
 		{
-			//cout << (*edges)[i] -> vertex_1 -> index << " " << (*edges)[i] -> vertex_2 -> index << endl;
 			v1-> incident_edges.push_back((*edges)[i]);
 			v2-> incident_edges.push_back((*edges)[i]);
 		}
-		else if(v1-> ramifications != 2 && v2->ramifications != 2)
+		else
 		{
 			available_edges.push_back((*edges)[i]); 
 		}
 	}
-	
-	for(list<Edge*>::iterator available_edge = available_edges.begin(); available_edge != available_edges.end(); ++available_edge)
-	{
-		list<Edge*> path;
-		Vertex *v1, *v2;
 
-		v1 = (*available_edge)-> vertex_1;
-		v2 = (*available_edge)-> vertex_2;
-		//cout << "FIND " << v1-> index << " " << v2-> index << endl;
-		find_path(v1, v2, &path);
-		change_edges( *available_edge, &path);
-	}
-	
+	while(improvement_1(&available_edges) || improvement_2(&available_edges));
+
 	for(int i = 0; i < n_vertices; i++)
 	{
 		if((*vertices)[i]-> variable)
